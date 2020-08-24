87a2c3f8ca4311a79bc35b53ea3e8a8b99eeebc6
Markos Alemu <markos@brandeis.edu>
2020-08-23 21:03:51 -0400

    first commit

---
+++ Hello.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.org'
+        self.gender = 'm'
+
+    def
+
+class Manager:
+    def __init__(self, level,pay):
+        self.level=level
+        self.pay=pay
+
+
